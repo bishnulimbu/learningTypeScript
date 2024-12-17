2b582e36b2cff951b93ef39d4f7ebd3456b152e3
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-12-17 13:02:09 +0545

    idk

--- or.js
+++ or.js
@@ -1,3 +1,7 @@
 var myCat = { id: 11, dog_name: "shepherd", gender: "male" };
-myCat = { id: myCat.id, cat_name: myCat.dog_name = "siberian", gender: myCat.gender };
+var newDog = { id: 1, dog_name: "kallay", gender: "female" };
+// myCat = {
+//   cat_name: mycat.dog_name = "siberian"
+// };
 console.log(myCat);
+console.log(newDog);
--- or.ts
+++ or.ts
@@ -11,8 +11,12 @@ type cat = {
 }
 
 let myCat: dog | cat = { id: 11, dog_name: "shepherd", gender: "male" };
+let newDog: dog = { id: 1, dog_name: "kallay", gender: "female" };
 
-myCat = {
-  cat_name: mycat.dog_name = "siberian"
-};
+// myCat = {
+//   cat_name: mycat.dog_name = "siberian"
+// };
+
 console.log(myCat);
+console.log(newDog);
+
